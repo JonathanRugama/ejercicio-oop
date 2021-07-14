fe2b90605f5e0e8edd53ad0ecffe8d175bd85139
Jonathan Rugama <39573472+JonathanRugama@users.noreply.github.com>
2021-07-14 17:54:12 -0600

    Comentario

--- Logic/Logic.cs
+++ Logic/Logic.cs
@@ -10,6 +10,8 @@ namespace PadronElectoralV2.Logic
 {
 	class Operations
 	{
+
+      // Implementar una Interfaz para esta clase --- Ilogic 
 		public Operations(ElectoralRoll theElectoralRoll)
 		{
 			TheElectoralRoll = theElectoralRoll;
